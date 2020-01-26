920236ee4abe00f066a5fec51d37f3ce145e31e1
James Hayes <jameshayes@MacbookPro-021.local>
2020-01-26 15:18:28 -0800

    initial

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by James Hayes on 1/26/20.
+//  Copyright © 2020 James Hayes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
